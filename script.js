54c9e7b0a0c0a7ccc80f3dfdb988b4d75e597637
Mart van den Berg <mart.vandenberg@coolblue.nl>
2022-06-16 16:39:51 +0200

    load confettie whit js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Add confettie blast
 const kaboem = document.createElement('img');
 kaboem.setAttribute('src', './assets/gif/kaboem.gif');
-kaboem.classList.add('confettie')
+kaboem.classList.add('confettie');
 
+// When everyrhing is loaded add kaboem to the page and start the animation timeline
 window.addEventListener('load',()=>{
     document.querySelector('.card__imgContainer').appendChild(kaboem);
 })
