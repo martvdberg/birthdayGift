1466a006a2883bd101e7ae7570e7dfbf3e5fcc84
Mart van den Berg <martvdberg@gmail.com>
2022-06-18 10:40:03 +0200

    added balloons and animatain for balloons

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const imgContainer = document.querySelector(".card__imgContainer");
-// Make confettie blast
 const kaboem = document.createElement("img");
-kaboem.setAttribute("src", "./assets/gif/kaboem.gif");
 kaboem.classList.add("confettie");
+imgContainer.appendChild(kaboem);
 
 document.querySelector(".playBtn").addEventListener("click", () => {
-  imgContainer.appendChild(kaboem);
+  kaboem.setAttribute("src", "./assets/gif/kaboem.gif");
+
   const cardTl = gsap.timeline();
   cardTl
     .add("startScreen")
@@ -33,6 +33,7 @@ document.querySelector(".playBtn").addEventListener("click", () => {
       ">+1.5"
     )
 
+    .to(".b7", { y: -500, duration: 4, ease: "linear" }, "<-4")
     .from(
       ".card__textContainer--1 .line2 .line__block",
       {
@@ -49,11 +50,13 @@ document.querySelector(".playBtn").addEventListener("click", () => {
     .to(".dove", { opacity: 0, duration: 0.2 }, ">")
     .to(".magicHat", { opacity: 0 }, "<")
 
+    // .to(".b1", { y: -500, duration: 4, ease: "linear" }, "<-4")
     .to(
       ".card__textContainer--1 .line2 .line__block",
       { opacity: 0, x: -40, duration: 1, stagger: 0.5 },
       "<"
     )
+
     .from(
       ".card__textContainer--1 .line3 .line__block",
       { opacity: 0, duration: 0.7, stagger: 0.7 },
@@ -65,8 +68,11 @@ document.querySelector(".playBtn").addEventListener("click", () => {
       { opacity: 0, y: -50, duration: 0.7, stagger: 0.2 },
       ">1.5"
     )
+    .to(".b2", { y: -500, duration: 4, ease: "linear" }, "<-4")
 
     .add("block_2")
+    .to(".b3", { y: -500, duration: 4, ease: "linear" }, "<-4")
+
     .from(
       ".card__textContainer--2 .line1",
       { opacity: 0, y: 30, duration: 1, ease: "bounce" },
@@ -90,17 +96,21 @@ document.querySelector(".playBtn").addEventListener("click", () => {
       { opacity: 0, stagger: 0, duration: 1 },
       ">+.5"
     )
+
     .from(".card__textContainer--3 .line1", { opacity: 0 }, ">+1")
+    .to(".b4", { y: -500, duration: 4, ease: "linear" }, "<-4")
     .from(
       ".card__textContainer--3 .line2 .line__block",
       { opacity: 0, scale: 0.6, stagger: 0.2, duration: 0.6, ease: "bounce" },
       ">"
     )
+
     .to(
       ".card__textContainer--3 .line2 .line__block",
       { opacity: 0, scale: 0.6, stagger: 0.2, duration: 0.6, ease: "bounce" },
       ">+2"
     )
+    .to(".b5", { y: -500, duration: 4, ease: "linear" }, "<-4")
     .from(
       ".card__textContainer--4 .header",
       { opacity: 0, y: -30, duration: 1 },
@@ -110,6 +120,46 @@ document.querySelector(".playBtn").addEventListener("click", () => {
       ".card__textContainer--4 .show",
       { opacity: 0, y: 20, stagger: 0.4 },
       "<+0.9"
-    );
-  // cardTl.play();
+    )
+
+    .from(
+      ".replayBtn",
+      { opacity: 0, rotation: -360, duration: 0.8, scale: 0 },
+      ">+1"
+    )
+    .to(".b6", { y: -500, duration: 4, ease: "linear" }, "<-4");
+});
+
+document.querySelector(".replayBtn").addEventListener("click", () => {
+  console.log(imgContainer);
+  const replayTl = gsap
+    .timeline()
+    .to(".card__startContainer", { opacity: 1 }, "startScreen")
+    .set(".card__startContainer", { zIndex: 100 })
+    .set(".card__headingContainer--text", { opacity: 1, scale: 1 })
+    .set(".name", { opacity: 1 })
+    .set(".age", { scale: 1, x: 0, y: 0 })
+    .set(".confettie", { opacity: 1 })
+    .set(".card__textContainer--1 .line1", { opacity: 1 })
+    .set(".card__textContainer--1 .line2 .line__block", { opacity: 1, x: 0 })
+    .set(".card__textContainer--1 .line3 .line__block", { opacity: 1, y: 0 })
+    .set(".card__textContainer--2 .line1", { opacity: 1 })
+    .set(".card__textContainer--2 .line2 .line__block", { opacity: 1 })
+    .set(".card__textContainer--3 .line1", { opacity: 1 })
+    .set(".card__textContainer--3 .line2 .line__block", {
+      opacity: 1,
+      scale: 1,
+    })
+    .set(".card__textContainer--4 .header", { opacity: 1, y: 0 })
+    .set(".card__textContainer--4 .show", { opacity: 1, y: 0 })
+    .set(".replayBtn", { opacity: 1 })
+    .set(".b1", { y: 0 })
+    .set(".b2", { y: 0 })
+    .set(".b3", { y: 0 })
+    .set(".b4", { y: 0 })
+    .set(".b5", { y: 0 })
+    .set(".b6", { y: 0 })
+    .set(".b7", { y: 0 });
+
+  kaboem.setAttribute("src", " ");
 });
